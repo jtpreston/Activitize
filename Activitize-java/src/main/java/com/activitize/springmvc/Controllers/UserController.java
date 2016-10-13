58f6ef851f7f428e2a650fcefd0dfb5c2f572fd4
Tyler Preston <prestoj@purdue.edu>
2016-10-13 17:13:21 -0400

    Added reply controller and fixed name of user controller

--- Activitize-java/src/main/java/com/activitize/springmvc/Controllers/UserController.java
+++ Activitize-java/src/main/java/com/activitize/springmvc/Controllers/UserController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/login")
-public class LoginController {
+@RequestMapping("/user")
+public class UserController {
     
 }
